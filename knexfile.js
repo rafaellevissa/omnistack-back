393b5fab3c0c4111a1e41ffc908bc2cbe1c16fe4
rafaellevissa <rafaellevissa@gmail.com>
2020-03-31 04:35:08 -0300

    teste

--- knexfile.js
+++ knexfile.js
@@ -39,7 +39,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgres',
+    client: 'postgres',    
     connection: 'postgres://xmszhcydqvsftf:7302d60196a7d91c30717db482d2cf6d16df2510a229121302960410a2ae85bc@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d717a90n7raibh',
     pool: {
       min: 2,
